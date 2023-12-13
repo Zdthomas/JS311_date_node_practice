6ebcb24ff036c21e6c197b878d1b2ae432d7e030
Zdthomas <thomaszachary@live.com>
2023-12-13 00:00:10 -0600

    Hub

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,17 +1,126 @@
 const isEven = (num) => {
   // write code for numbers.isEven
 
+  return num % 2 === 0;
+
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
 
+  let sum = 0; 
+
+    for(var i = 0; i < arr.length; i++) {
+
+      if(typeof arr[i] === `number`) sum += arr[i]
+    }
+
+    return sum
+
+  
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
 
+
+  const findCombination = (index, currentSum) => {
+
+    if (currentSum === sum) {
+
+      return true; // Combination found
+
+    }
+
+    if (index === arr.length) {
+
+      return false; 
+      // Reached the end of the array without finding a combination
+
+    }
+
+    // Include the current element in the combination
+
+    if (findCombination(index + 1, currentSum + arr[index])) {
+
+      return true;
+
+    }
+
+    // Exclude the current element from the combination
+    return findCombination(index + 1, currentSum);
+    
+  };
+
+  return false;
 }
+
+
+
+
+  // const result = [];
+
+  // const findCombo = (combo, arr, sum) => {
+
+  //   if (sum === 0) {
+
+  //     result.push([...combo]);
+  //     return
+
+  //   }
+
+  //   for (let i = 0; i < arr.length; i++) {
+
+  //     const num = arr[i];
+
+  //     if (num <= sum) {
+
+  //       combo.push(num);
+
+  //       findCombo(combo, arr.slice(i), sum - num);
+
+  //       combo.pop();
+
+  //     }
+
+  //   }
+
+  // };
+
+  
+  // return result;
+  
+
+
+
+  
+
+
+
+
+
+
+  // let arr = [1, 2, 3, 6, 8, 9]
+
+  // let newArr = []
+
+  // let sum = 0
+
+  // let result = 24
+
+  // if (sum === result) {
+
+  //   result.push(newArr);
+
+  //   return
+
+  // }
+  
+  
+
+
+
+
 
 module.exports = {
   isEven,
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,15 +1,31 @@
 const split = (str, delim) => {
-  // write code for strings.split
+  
+  return str.split(delim);
+
 
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  const result = [];
+
+  for (let i = 0; i < str.length - 1; i += 2) {
+
+    const pair = str.slice(i, i + 2);
+
+    result.push(pair);
+
+  }
+
+  return result;
+  
 
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
+  return str.split('').reverse().join('');
+
 
 }
 
